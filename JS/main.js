ca0b0907a4554a4c28f84a43d1ae65418a97210c
NicolaTeora <nikxodia@gmail.com>
2024-01-10 19:55:40 +0100

    feat : not progress !!

--- JS/main.js
+++ JS/main.js
@@ -6,28 +6,32 @@ let discountPrice;
 const discount20 = 0.2;
 const discount40 = 0.4;
 const priceKm = 0.21;
+const submit = document.getElementById('submit');
 
 //assegnazione
-//eta passeggero
+//età passeggero
 //chilometri da percorrere
 
-userAge = document.getElementById('user-age');
+userAge = document.getElementById('user-age').value;
 console.log(userAge);
-
-mileage = document.getElementById('distance');
+mileage = document.getElementById('distance').value;
 console.log(mileage);
 
-//prezzo standard
-
-fristPrice = mileage * priceKm;
-console.log(fristPrice);
-
-//scontistica da applicare al prezzo standard in base all'età
-
-if (userAge < 18) {
-    discountPrice = fristPrice - (fristPrice * discount20);
-}
-
-if (userAge > 65) {
-    discountPrice = fristPrice - (fristPrice * discount40);
-}
+submit.addEventListener('click', function(){
+    
+    //prezzo standard
+    fristPrice = mileage * priceKm;
+    console.log(fristPrice);
+    
+    document.getElementById('standarPrice').innerText = fristPrice + '€'
+    
+    //scontistica da applicare al prezzo standard in base all'età
+    
+    if (userAge < 18) {
+        discountPrice = fristPrice - (fristPrice * discount20);
+    }
+    
+    if (userAge > 65) {
+        discountPrice = fristPrice - (fristPrice * discount40);
+    }
+});
