5255b0b19f9e6283c8e94febfe2e9d685092881d
NicolaTeora <nikxodia@gmail.com>
2024-01-10 17:25:14 +0100

    feat : develop main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,33 @@
-console.log('online')
+// dichiarazione variabili
+let userAge;
+let mileage;
+let fristPrice;
+let discountPrice;
+const discount20 = 0.2;
+const discount40 = 0.4;
+const priceKm = 0.21;
+
+//assegnazione
+//eta passeggero
+//chilometri da percorrere
+
+userAge = document.getElementById('user-age');
+console.log(userAge);
+
+mileage = document.getElementById('distance');
+console.log(mileage);
+
+//prezzo standard
+
+fristPrice = mileage * priceKm;
+console.log(fristPrice);
+
+//scontistica da applicare al prezzo standard in base all'età
+
+if (userAge < 18) {
+    discountPrice = fristPrice - (fristPrice * discount20);
+}
+
+if (userAge > 65) {
+    discountPrice = fristPrice - (fristPrice * discount40);
+}
