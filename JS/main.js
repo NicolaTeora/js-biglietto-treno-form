fec2320a16f715594a1fee9e6e762c1387e66708
NicolaTeora <nikxodia@gmail.com>
2024-04-24 12:06:46 +0200

    fix: main.js

--- JS/main.js
+++ JS/main.js
@@ -7,43 +7,38 @@ let userName;
 const discount20 = 0.2;
 const discount40 = 0.4;
 const priceKm = 0.21;
-const submit = document.getElementById('submit');
+const submit = document.getElementById("submit");
 
 //assegnazione
 //età passeggero
 //chilometri da percorrere
-submit.addEventListener('click', function() {
+submit.addEventListener("click", function () {
+  userAge = document.getElementById("user-age").value;
+  console.log(userAge);
+  mileage = document.getElementById("distance").value;
+  console.log(mileage);
+  userName = document.getElementById("nameUser");
+  console.log(userName);
 
-userAge = document.getElementById('user-age').value;
-console.log(userAge);
-mileage = document.getElementById('distance').value;
-console.log(mileage);
-userName = document.getElementById('nameUser');
-console.log(userName);
+  let resumeDetailTicket = document.getElementById("ticketScreen");
+  resumeDetailTicket.classList.remove("d-none");
 
-    //prezzo standard
-    fristPrice = mileage * priceKm;
-    console.log(fristPrice);
-    
-    document.getElementById('standarPrice').innerText = fristPrice + '€'
-    
-    //scontistica da applicare al prezzo standard in base all'età
-    
-    // if (userAge < 18) {
-    //     discountPrice = fristPrice - (fristPrice * discount20);
-    // } else if (userAge > 65) {
-    //     discountPrice = fristPrice - (fristPrice * discount40);
-    // }
-    // console.log(discountPrice);
+  //prezzo standard
+  fristPrice = mileage * priceKm;
 
-    if (userAge == "minorenne") {
-        discountPrice = fristPrice - (fristPrice * discount20);
-    } else if (userAge == "over65") {
-        discountPrice = fristPrice - (fristPrice * discount40);
-    }
-    console.log(discountPrice);
-    
-    document.getElementById('discountTicket').innerText = discountPrice;
-    document.getElementById('userticket').innerText = userName.value;
-    console.log(userticket);
+  document.getElementById("standarPrice").innerText = fristPrice + "€";
+
+  //scontistica da applicare al prezzo standard in base all'età
+
+  if (userAge == "minorenne") {
+    discountPrice = fristPrice - fristPrice * discount20;
+  } else if (userAge == "over65") {
+    discountPrice = fristPrice - fristPrice * discount40;
+  } else {
+    discountPrice = "";
+  }
+
+  document.getElementById("discountTicket").innerText = discountPrice;
+  document.getElementById("userticket").innerText = userName.value;
+  console.log(userticket);
 });
